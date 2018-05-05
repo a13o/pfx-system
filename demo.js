d28e5fa4fffcf29a415ac6a6bfdc198576b4175b
Andrew Pellerano <2031379+a13o@users.noreply.github.com>
2018-05-05 10:58:19 -0700

    Use linked lists instead of arrays for particle management

--- demo.js
+++ demo.js
@@ -2,15 +2,17 @@ import PfxSystem from './src/pfx-system.js';
 import RainConfig from './src/config/pfx-config-rain.js';
 
 const pfx = new PfxSystem();
+
+const freq = 4000;
 pfx.addConfig(RainConfig, {
   colors: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
-  frequency: 4000, // pfx/sec
+  frequency: freq, // pfx/sec
   lifespan: 800, // px
   lifespanVariance: 0.05, // %
   size: 7, // px
-  velocity: 800, // px/sec
+  velocity: 600, // px/sec
   velocityVariance: 0.5, // %
-});
+}, freq * 1.25);
 
 pfx.start();
 
--- src/config-pool.js
+++ src/config-pool.js
@@ -5,9 +5,19 @@ export default class ConfigPool {
 
     this.ParticleClass = config.Particle;
 
-    this.pool = [];
+    this.pool = {
+      __head: true,
+      __next: null,
+    };
+    this.active = {
+      __head: true,
+      __next: null,
+    };
+
+    let curr = this.pool;
     for (let i = 0; i < initialSize; i++) {
-      this.pool.push(this.createParticle());
+      curr.__next = this.createParticle();
+      curr = curr.__next;
     }
 
     this.api = {
@@ -15,45 +25,50 @@ export default class ConfigPool {
       despawn: this.despawn.bind(this),
       context: canvas.getContext('2d'),
     };
-
-    this.active = [];
   }
 
   createParticle() {
     const p = new this.ParticleClass();
     p.__markForDelete = false;
+    p.__next = null;
     return p;
   }
 
   step(dt) {
-    let delCount = 0;
-    for (let i = 0; i < this.active.length; i++) {
-      const p = this.active[i];
-      if (p.__markForDelete === true) {
-        delCount += 1;
-        p.__markForDelete = false;
-        this.pool.push(p);
+    let curr = this.active.__next;
+    let prev = this.active;
+    while(curr !== null) {
+      if (curr.__markForDelete === true) {
+        curr.__markForDelete = false;
+
+        // slice from active list
+        prev.__next = curr.__next;
+
+        // add to front of pool
+        curr.__next = this.pool.__next;
+        this.pool.__next = curr;
+
+        // set the loop pointer
+        curr = prev.__next;
       } else {
-        p.step(dt, this.api);
-        if (delCount > 0) {
-          this.active[i - delCount] = p;
-        }
+        curr.step(dt, this.api);
+        prev = curr;
+        curr = curr.__next;
       }
     }
-    this.active.length -= delCount;
 
     this.system.step(dt, this.api);
   }
 
   spawn(options) {
-    const last = this.pool.length - 1;
-    if (last < 0) {
+    const p = this.pool.__next;
+
+    if (p === null) {
       // TODO: set a starvation flag for the debug layer to report on
       return;
     }
 
-    const p = this.pool[last];
-    this.pool.length = last;
+    this.pool.__next = p.__next;
 
     if (!this.optionsKeys) {
       this.optionsKeys = Object.keys(options);
@@ -64,7 +79,8 @@ export default class ConfigPool {
       p[key] = options[key];
     }
 
-    this.active.push(p);
+    p.__next = this.active.__next;
+    this.active.__next = p;
   }
 
   despawn(p) {
